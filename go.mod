1adc537555bd42cabcca06f1280db299b92cab86
Marco Siebecke <marco@siebecke.se>
2021-11-18 22:45:59 +0100

    send data to influxcloud

--- go.mod
+++ go.mod
@@ -6,7 +6,14 @@ require (
 	github.com/alecthomas/kong v0.2.18
 	github.com/gentoomaniac/gocli v0.0.0-20210503153723-bf433779ccf3
 	github.com/gentoomaniac/logging v0.0.2
+	github.com/influxdata/influxdb-client-go/v2 v2.5.1
 	github.com/rs/zerolog v1.26.0
 )
 
-require github.com/pkg/errors v0.9.1 // indirect
+require (
+	github.com/deepmap/oapi-codegen v1.8.2 // indirect
+	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"net/url"
+	"strconv"
+	"strings"
+
 	"github.com/alecthomas/kong"
 	"github.com/rs/zerolog/log"
 
 	"github.com/gentoomaniac/gocli"
 	"github.com/gentoomaniac/logging"
+
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 )
 
 var (
@@ -19,10 +26,46 @@ var (
 var cli struct {
 	logging.LoggingConfig
 
-	APIKey string   `help:"API key for coinmarketcap.com" short:"k" required:""`
-	Coin   []string `help:"coins to fetch data for" short:"c" required:""`
+	Coin                 []string `help:"coins to fetch data for and its values: <slug>:<buy_price>:<amount>" short:"c" required:""`
+	CoinmarketplaceToken string   `help:"API key for coinmarketcap.com" required:""`
+	InfluxToken          string   `help:"API token for influxcloud" required:""`
+	InfluxURL            *url.URL `help:"url of influxdb" default:"https://eu-central-1-1.aws.cloud2.influxdata.com"`
+	InfluxOrg            string   `help:"influxdb org name" required:""`
+	InfluxBucket         string   `help:"influxdb bucket name" required:""`
 
 	Version gocli.VersionFlag `short:"V" help:"Display version."`
+}
+
+func NewCoinData(parameter string) *CoinData {
+	c := &CoinData{}
+
+	fields := strings.Split(parameter, ":")
+	if len(fields) != 3 {
+		log.Error().Str("parameter", parameter).Msg("incorrect number of fields")
+		return nil
+	}
+
+	var err error
+	c.Slug = fields[0]
+	c.BuyPrice, err = strconv.ParseFloat(fields[1], 64)
+	if err != nil {
+		log.Error().Str("buyPrice", fields[1]).Msg("field is not a float")
+		return nil
+	}
+
+	c.AmountOwned, err = strconv.ParseFloat(fields[2], 64)
+	if err != nil {
+		log.Error().Str("amountOwned", fields[2]).Msg("field is not a float")
+		return nil
+	}
+
+	return c
+}
+
+type CoinData struct {
+	Slug        string
+	BuyPrice    float64
+	AmountOwned float64
 }
 
 func main() {
@@ -35,21 +78,43 @@ func main() {
 	})
 	logging.Setup(&cli.LoggingConfig)
 
-	c, err := NewCoinmarketcap(cli.APIKey, nil)
+	coinData := make(map[string]*CoinData)
+	var slugs []string
+	for _, coin := range cli.Coin {
+		cd := NewCoinData(coin)
+		coinData[cd.Slug] = cd
+		slugs = append(slugs, cd.Slug)
+	}
+
+	c, err := NewCoinmarketcap(cli.CoinmarketplaceToken, nil)
 	if err != nil {
 		log.Error().Err(err).Msg("failed to set up coinmarketcap client")
 		ctx.Exit(1)
 	}
 
-	coins, err := c.GetQuotesLatest(cli.Coin)
+	coins, err := c.GetQuotesLatest(slugs)
 	if err != nil {
 		log.Error().Err(err).Msg("getting data failed")
 		ctx.Exit(1)
 	}
 
+	client := influxdb2.NewClient(cli.InfluxURL.String(), cli.InfluxToken)
+	defer client.Close()
+	writeAPI := client.WriteAPI(cli.InfluxOrg, cli.InfluxBucket)
+
 	for _, coin := range coins.Data {
 		log.Info().Str("name", coin.Name).Float64("price", coin.Quote["USD"].Price).Time("lastUpdate", coin.Quote["USD"].LastUpdated).Msg("")
+		recordString := fmt.Sprintf(
+			"%s,slug=%s,name=%s price=%f,buy=%f,amount=%f",
+			coin.Symbol,
+			coin.Slug, strings.ReplaceAll(coin.Name, " ", "\\ "),
+			coin.Quote["USD"].Price,
+			coinData[coin.Slug].BuyPrice,
+			coinData[coin.Slug].AmountOwned,
+		)
+		writeAPI.WriteRecord(recordString)
 	}
+	writeAPI.Flush()
 
 	ctx.Exit(0)
 }
